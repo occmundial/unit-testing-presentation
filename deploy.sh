298acddcc2d2c2c2f952b24e28fa94c525dfd2f5
PepeFranco <gfranco@occ.com.mx>
2017-10-10 18:21:14 -0500

    Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 docker service rm $WA_UNITTESTING_APP_NAME
-docker build -t $WA_UNITTESTING_APP_NAME:latest -t $WA_UNITTESTING_APP_NAME:$WA_UNITTESTING_APP_VERSION .
 docker service create \
    --name $WA_UNITTESTING_APP_NAME \
    --replicas $WA_UNITTESTING_REPLICAS \
