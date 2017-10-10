84bafb6f1e43182c5007085a17fc6d67f11fddd5
PepeFranco <gfranco@occ.com.mx>
2017-10-10 14:01:37 -0500

    server js port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = require("./webpack.config");
 var app = express();
 var compiler = webpack(config);
 
-var serverPort = process.env.PORT || 3000;
+var serverPort = process.env.PORT || #WA_UNIT_TESTING_PORT#;
 
 app.use(require("webpack-dev-middleware")(compiler, {
   noInfo: true,
